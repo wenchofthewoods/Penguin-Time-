99999658ead8c822dc0a9bb9bb98a74ffaa5d256
wenchofthewoods <150164887+wenchofthewoods@users.noreply.github.com>
2023-12-08 02:07:40 +0000

    Add files via upload

---
+++ Penguins_Project/Functions/libraries.r
@@ -0,0 +1,27 @@
+## ---------------------------
+##
+## Script name: libraries.r
+##
+## Purpose of script: 
+##      Loading necessary libraries
+##
+## Author: Dr. Lydia France
+##
+## Date Created: 2022-11-10
+##
+##
+## ---------------------------
+##
+## Notes:
+##   Feel free to use this script to load the libraries you need for your project.
+##   In your own script, you use the source("functions/libraries.r") function to load this script.
+##
+## ---------------------------
+
+library(palmerpenguins)
+library(ggplot2)
+suppressPackageStartupMessages(library(janitor))
+suppressPackageStartupMessages(library(dplyr))
+library(tidyr)
+library(ragg)
+library(svglite)
